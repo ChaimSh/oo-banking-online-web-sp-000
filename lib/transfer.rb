a9a18e9cb4e96cc9bf32bb5b59e68798f2b4e7a7
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-05-09 02:36:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ end
 end
 
 def execute_transaction
-  if self.valid? == true && sender.balance > amount && self.status == "pending"
+  if self.valid? && sender.balance > amount && self.status == "pending"
     sender.balance -= amount
     receiver.balance += amount
     self.status = "complete"
