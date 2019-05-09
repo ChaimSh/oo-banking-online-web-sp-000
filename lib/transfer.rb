4465615309a62a4d81b4ed1737f1f9269911a9d3
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-05-09 02:11:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
 class Transfer
-  # your code here
+  attr_accessor :transfer, :sender, :reciever, :status
+
+  def initialize(sender, reciever)
+  @sender = sender
+  @reciever = reciever
+  @status = "pending"
+  @transfer = 1000
+  end
+
+
+
+
+
+
+
+
+
 end
